3d7785958045b14fb29184e9b506566d9e40b2b9
lamhiendev <enderbossvn@gmail.com>
2026-04-07 09:33:49 +0700

    first commit

---
+++ HW2.sql
@@ -0,0 +1,38 @@
+CREATE TABLE accounts (
+    account_id SERIAL PRIMARY KEY,
+    owner_name VARCHAR(100),
+    balance NUMERIC(10, 2)
+);
+
+INSERT INTO accounts (owner_name, balance)
+VALUES ('A', 500.00), ('B', 300.00);
+
+BEGIN;
+
+UPDATE accounts 
+SET balance = balance - 100.00 
+WHERE owner_name = 'A';
+
+UPDATE accounts 
+SET balance = balance + 100.00 
+WHERE owner_name = 'B';
+
+COMMIT;
+
+SELECT * FROM accounts;
+-- Kết quả dự kiến: A còn 400.00, B có 400.00
+
+BEGIN; 
+
+UPDATE accounts 
+SET balance = balance - 100.00 
+WHERE owner_name = 'A';
+
+UPDATE accounts 
+SET balance = balance + 100.00 
+WHERE account_id = 999; 
+
+ROLLBACK;
+
+
+SELECT * FROM accounts WHERE owner_name = 'A';
